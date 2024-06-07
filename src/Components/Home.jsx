f76ca37654d101d7e6b2fa7ee0994267043e965e
Ananya Juneja <junejaananya1234@gmail.com>
2024-06-08 00:23:51 +0530

    done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,4 @@
 import Navbar from "./Navbar";
-import video from '../../assets/BGA.mp4';
 import HandleUpload from "./HandleUpload";
 
 const Home = () => {
@@ -9,16 +8,7 @@ const Home = () => {
             <div className="flex flex-col min-h-screen w-full items-center">
 
                 <div className="flex w-[80%] items-center justify-center p-4 gap-2">
-                    <video
-                        className="w-64 object-cover"
-                        preload="auto"
-                        autoPlay
-                        muted
-                        playsInline
-                    >
-                        <source src={video} type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
+                    
                 </div>
                 <div className="flex flex-col items-center mt-4">
                     <p className="text-3xl w-full font-semibold text-slate-600 text-center">
